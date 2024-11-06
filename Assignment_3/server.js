e3a608db7b2d06e529d16b9a3952447eb9b83409
Ali <153542050+Alikhk1@users.noreply.github.com>
2024-11-06 16:57:05 +0500

    Assignment 3
    Updated with images on portfolio page and some code tweaks

--- Assignment_3/server.js
+++ Assignment_3/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 let server = express();
 
+// Set the view engine to EJS
 server.set("view engine", "ejs");
 
+// Serve static files from the 'public' folder
 server.use(express.static("public"));
 
+// Route for the '/about-me' page
 server.get("/about-me", (req, res) => {
-  res.render("porfolio");
-});
-server.get("/", (req, res) => {
-  res.send(res.render("Apple"));
+  res.render("portfolio"); // Make sure the file name is portfolio.ejs
 });
 
+// Route for the home page
+server.get("/", (req, res) => {
+  res.render("Apple"); // Render Apple.ejs file
+});
+
+// Start the server
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
